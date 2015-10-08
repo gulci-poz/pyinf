e0183377bed6a20b3542ee2c7c36ec69ed446a7c
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2015-10-08 23:10:51 +0200

    2015-10-08 chapter 5.1 finished

---
+++ ex309.py
@@ -0,0 +1,13 @@
+# py2 wydrukuje jako tuple
+# py3 wydrukuje jako string
+
+print('hello', 100, 'boss')
+
+word = 'hello'
+number = 100
+
+print(word, number)
+
+name = "gulci"
+grade = 99.91111
+print("%s: %.2f" % (name, grade))
